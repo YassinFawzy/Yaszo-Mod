3c3b53321800c35b2320045262c953688da5985f
Yasz0 <yassinfawzycanada@gmail.com>
2023-07-16 21:26:41 +0300

    Added Zircon Pearl and creative mode tab

--- src/main/java/net/yassin/yaszomod/item/ModItems.java
+++ src/main/java/net/yassin/yaszomod/item/ModItems.java
@@ -1,6 +1,6 @@
 package net.yassin.yaszomod.item;
 
-import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.Items;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -14,9 +14,11 @@ public class ModItems {
             DeferredRegister.create(ForgeRegistries.ITEMS, YaszoMod.MOD_ID);
 
     public static final RegistryObject<Item> ZIRCON= ITEMS.register("zircon",
-            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.YASZO_TAB)));
     public static final RegistryObject<Item> RAW_ZIRCON= ITEMS.register("raw_zircon",
-            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.YASZO_TAB)));
+    public static final RegistryObject<Item> ZIRCON_PEARL= ITEMS.register("zircon_pearl",
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.YASZO_TAB).stacksTo(128)));
 
     public static void register(IEventBus eventBus){
 
