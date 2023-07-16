a8ce792c7a871c9e9a17d3fc9394fd6239558fba
Yasz0 <yassinfawzycanada@gmail.com>
2023-07-16 03:45:47 +0300

    Setting up mod

---
+++ src/main/java/net/yassin/yaszomod/item/ModItems.java
@@ -0,0 +1,24 @@
+package net.yassin.yaszomod.item;
+
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+import net.yassin.yaszomod.YaszoMod;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS=
+            DeferredRegister.create(ForgeRegistries.ITEMS, YaszoMod.MOD_ID);
+
+    public static final RegistryObject<Item> YASZON= ITEMS.register("yaszon",
+            ()-> new Item(new Item.Properties().stacksTo(128).durability(50).tab(CreativeModeTab.TAB_MISC)));
+    public static final RegistryObject<Item> RAW_YASZON= ITEMS.register("raw_yaszon",
+            ()-> new Item(new Item.Properties().stacksTo(128).durability(50).tab(CreativeModeTab.TAB_MISC)));
+
+
+    public static void register(IEventBus eventBus){
+        ITEMS.register(eventBus);
+    }
+}
